b2cc07c7d27387ac0a2634741420a1f3bd8572c7
dolhim <eadorim@naver.com>
2016-09-06 15:17:13 +0900

    insert row dynamic at layer list control

--- drlee_v3-demo.js
+++ drlee_v3-demo.js
@@ -1,6 +1,25 @@
-  
+
+
+      /* 레이어 테이블 생성 */
+      function setLayerList(layerlist) {
+        var table = document.getElementById('layertable');
+        table.innerHTML = "<tr><th>ㅁ</th><th>레이어</th><th>opacity</th></tr>";
+
+        layerlist.forEach(function(layer, i) {
+          bindInputs(i, layer);
+          if (layer instanceof ol.layer.Group) {
+            layer.getLayers().forEach(function(sublayer, j) {
+              bindInputs(String(i) + String(j), sublayer);
+            });
+          }
+        });
+      }
+
       /* 레이어 속성 연결 */
       function bindInputs(id, layer) {
+
+        addRow(id, layer);
+
         var visibilityInput = $('#visible' + id);
         visibilityInput.on('change', function() {
           layer.setVisible(this.checked);
@@ -14,6 +33,17 @@
         opacityInput.val(String(layer.getOpacity()));
       }
 
+      function addRow(idx, layer) {
+        var table = document.getElementById("layertable");
+        var row = table.insertRow(table.rows.length);
+        cell0 = row.insertCell(0);
+        cell1 = row.insertCell(1);
+        cell2 = row.insertCell(2);
+        cell0.innerHTML = '<input id="visible' + idx + '" class="visible" type="checkbox"/>';
+        cell1.innerHTML = '<td>' + layer.get('name') + '</td>';
+        cell2.innerHTML = '<td><input id="opacity' + idx + '" type="range" min="0" max="1" step="0.01"/>';
+      }        
+
       /* 사이드 내비 메뉴 열기 */
       function openNav() {
         document.getElementById("leftsidenav").style.width = "250px";
@@ -23,7 +53,6 @@
       function closeNav() {
         document.getElementById("leftsidenav").style.width = "0px";
       }
-
 
       /* 위치로 이동 */
       function panTo(location) {
